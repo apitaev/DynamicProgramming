8284e628da8047c4a4e513059bd0e39311e9ff93
Anna Pitaev <anna.pitaev@gmail.com>
2025-04-19 10:32:56 -0700

    Added implementation for the UniquePathsWithObstacles problem

--- src/main/java/com/pitaev/leetcode/dp/UniquePathsWithObstacles.java
+++ src/main/java/com/pitaev/leetcode/dp/UniquePathsWithObstacles.java
@@ -5,7 +5,35 @@ package com.pitaev.leetcode.dp;
  * https://leetcode.com/problems/unique-paths-ii/
  */
 public class UniquePathsWithObstacles {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            // number of rows in a grid
+            int m = obstacleGrid.length;
+            // number of columns in a grid
+            int n = obstacleGrid[0].length;
+            // table[i][j] number of max number paths from the cell 0,0 to the cell i,j
+            int[][] table = new int[m][n];
+            // compute max number of paths for the top left corner
+            // if obstacle found in the top left corner, return 0
+            if (obstacleGrid[0][0] == 1) return 0;
+            table[0][0] = 1;
+            // compute max  number of paths for the first row
+            for (int i = 1; i < n; i++) {
+                if (obstacleGrid[0][i] == 1) break;
+                table[0][i] = 1;
+            }
+            // compoute max number of paths for the first column
+            for (int i = 1; i < m; i++) {
+                if (obstacleGrid[i][0] == 1) break;
+                table[i][0] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    // if cell is obstacle, number of paths is 0
+                    if (obstacleGrid[i][j] == 1) continue;
+                    // if cell is free, you can reach it from left and from top
+                    table[i][j] = table[i][j - 1] + table[i - 1][j];
+                }
+            }
+            return table[m - 1][n - 1];
+        }
 }
