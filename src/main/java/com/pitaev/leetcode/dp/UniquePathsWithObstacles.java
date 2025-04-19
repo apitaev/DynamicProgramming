e02f7dd47f796aa5bab8de1c647141a182cc9f31
Anna Pitaev <anna.pitaev@gmail.com>
2025-04-19 10:05:36 -0700

    Prepare for the initial commit

---
+++ src/main/java/com/pitaev/leetcode/dp/UniquePathsWithObstacles.java
@@ -0,0 +1,11 @@
+package com.pitaev.leetcode.dp;
+
+/**
+ * This class implements a solution for a leetcode problem <code>63</code>:
+ * https://leetcode.com/problems/unique-paths-ii/
+ */
+public class UniquePathsWithObstacles {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
