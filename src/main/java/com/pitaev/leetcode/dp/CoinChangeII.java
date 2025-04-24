c3fd24c324d9a7b74b8bd3c7254a0ed431caa709
Anna Pitaev <anna.pitaev@gmail.com>
2025-04-24 16:00:02 -0700

    Added implementation for a Fibonaccin Numbers

---
+++ src/main/java/com/pitaev/leetcode/dp/CoinChangeII.java
@@ -0,0 +1,10 @@
+package com.pitaev.leetcode.dp;
+
+/**
+ * This class implements a solution for a leetcode problem <code>518</code>:
+ * <a href="https://leetcode.com/problems/coin-change-ii/">Partition Equal Subset Sum</a>
+ * <p>Time complexity: O(m * n), where m is a length of the word1 and n is a length of the word 2
+ * <p>Space complexity: O(m * n ), size of the table
+ */
+public class CoinChangeII {
+}
---
+++ src/main/java/com/pitaev/leetcode/dp/FibonacciNumber.java
@@ -0,0 +1,20 @@
+package com.pitaev.leetcode.dp;
+
+/**
+ * This class implements a solution for a leetcode problem <code>509</code>:
+ * <a href="https://leetcode.com/problems/fibonacci-number/description/">Fibonacci Number</a>
+ * <p>Time complexity: O(n)
+ * <p>Space complexity: O(1), we only use two previous columns
+ */
+public class FibonacciNumber {
+    public int fib(int n) {
+        if (n == 0 || n == 1) return n;
+        int [] table = new int [2];
+        table[0] = 0;
+        table[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            table[i % 2] = table[(i - 1) % 2] + table[(i - 2) % 2];
+        }
+        return table[n % 2];
+    }
+}
