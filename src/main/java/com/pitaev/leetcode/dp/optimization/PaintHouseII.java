38284c80810647582d193f52fbd568e83018f29a
Anna Pitaev <anna.pitaev@gmail.com>
2025-05-12 19:55:44 -0400

    Updated solution for the paint house problem

--- src/main/java/com/pitaev/leetcode/dp/optimization/PaintHouseII.java
+++ src/main/java/com/pitaev/leetcode/dp/optimization/PaintHouseII.java
@@ -5,45 +5,57 @@ import java.util.Arrays;
 /**
  * This class implements a solution for a leetcode problem <code>265</code>:
  * <a href="https://leetcode.com/problems/paint-house-ii/description/">Paint House II</a>
- * <p>Time complexity: O(n * k ^ 2)
- * <p>Space complexity: O(n * K), can be improved by using two rows.
+ * <p>Time complexity: O(n * k)
+ * <p>Space complexity: O(k).
  */
 public class PaintHouseII {
     public int minCostII(int[][] costs) {
-        // f(i, c) min cost to paint ith houses ending with color c
-        int [][] table = new int [costs.length][costs[0].length];
         // edge case: if number of houses is 1, return min of c1..ck
         if (costs.length == 1) return Arrays.stream(costs[0]).min().getAsInt();
+        // f(i, c) min cost to paint 1..i houses ending with color c
+        int [][] table = new int [2][costs[0].length];
+        // min cost to paint the Ith house
+        int minI = Integer.MAX_VALUE;
+        // second min cost to paint the Ith house
+        int secondMinI = Integer.MAX_VALUE;
         //base case: paint the first house
         for (int i = 0; i < costs[0].length; i++) {
             table[0][i] = costs[0][i];
+            if (costs[0][i] < minI) {
+                // set second min value to the absolute min value
+                secondMinI = minI;
+                // update absolute / first min value;
+                minI = costs[0][i];
+            } else if (costs[0][i] < secondMinI) {
+                secondMinI = costs[0][i];
+            }
         }
-        // recursive case: paint ith house with color ci, provided the house (i - 1)th is already painted
+        // recursive case: paint ith house with color cith, provided the house (i - 1)th is already painted
         // f(i, ci) = costs[i][ci] + min(costs[i - 1][c1], ..,costs[i - 1][ck]), so that c1..ck != ci
         for (int i = 1; i < costs.length; i++) {
+            // min cost to paint current house (ith house)
+            int minCurrent = Integer.MAX_VALUE;
+            // second min cost to paint current house (ith house)
+            int secondMinCurrent = Integer.MAX_VALUE;
             for (int j = 0; j < costs[0].length; j++) {
-                table[i][j] = costs[i][j] + getMin(table, i - 1, j);
+                // if min value is of the same color as my current cell use the second min value
+                int minCostForLastHouse = table[(i - 1) % 2][j] == minI ? secondMinI : minI;
+                table[i % 2][j] = (table[(i - 1) % 2][j] == minI ? secondMinI : minI) + costs[i][j];
+                if (table[i % 2][j] < minCurrent) {
+                    // update second minimum to the first minimum
+                    secondMinCurrent = minCurrent;
+                    // update the first minimum
+                    minCurrent = table[i % 2][j];
+                } else if (table[i % 2][j] < secondMinCurrent) {
+                    // update the second minimum value
+                    secondMinCurrent = table[i % 2][j];
+                }
             }
+            minI = minCurrent;
+            secondMinI = secondMinCurrent;
         }
         // return the min for the last houses costs
-        Integer min = Integer.MAX_VALUE;
-        for (int i = 0; i < costs[0].length; i++) {
-            if (table[costs.length - 1][i] < min) {
-                min = table[costs.length - 1][i];
-            }
-        }
-        return min;
+        return minI;
     }
-
-    private static Integer getMin(int [][] table, int row, int columnToExclude) {
-        Integer min = Integer.MAX_VALUE;
-        for (int i = 0; i < table[0].length; i++) {
-            if (i == columnToExclude) continue;
-            if (table[row][i] < min) {
-                min = table[row][i];
-            }
-        }
-        return min;
-    }
-
 }
+
---
+++ src/main/java/com/pitaev/leetcode/dp/optimization/ShortestCommonSupersequence.java
@@ -0,0 +1,10 @@
+package com.pitaev.leetcode.dp.optimization;
+
+/**
+ * This class implements a solution for a leetcode problem <code>1092</code>:
+ * <a href="https://leetcode.com/problems/shortest-common-supersequence/description/">Shortest Common Supersequence </a>
+ * <p>Time complexity:
+ * <p>Space complexity:
+ */
+public class ShortestCommonSupersequence {
+}
