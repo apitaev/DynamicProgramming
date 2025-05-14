82286b389e5a4d70eb1cf3d5b3120282d27c2a83
Anna Pitaev <anna.pitaev@gmail.com>
2025-05-13 20:22:17 -0400

    Added a single table DP solution for the DominoAndTromino problem

--- src/main/java/com/pitaev/leetcode/dp/counting/DominoAndTrominoSingleTable.java
+++ src/main/java/com/pitaev/leetcode/dp/counting/DominoAndTrominoSingleTable.java
@@ -1,26 +1,20 @@
 package com.pitaev.leetcode.dp.counting;
 
+
 /**
  * This class implements a solution for a leetcode problem <code>790</code>:
  * <a href="https://leetcode.com/problems/domino-and-tromino-tiling/description/">r</a>
  * <p>Time complexity: O(n)
  * <p>Space complexity: O(n)
- * Observations:
- * <ul>
- *     <li>This solution uses 3 tables</li>
- *     <li>We need one table to model a shape with extra low square</li>
- *     <li>We need one table to model a shape with extra upper square</li>
- * </ul>
  */
-public class DominoAndTromino {
+public class DominoAndTrominoSingleTable {
     public int numTilings(int n) {
-        if (n == 1) return 1;
+        if (n == 1 || n == 2) return n;
+        if (n == 3) return 5;
         // fTable defines number of Tilings for 2 X n shape
-        long[] fTable = new long[n + 1];
+        long [] fTable = new long[n + 1];
         // lTable defines a number of Tilings for a shape 2 X n with extra low squre
-        long[] lTable = new long[n + 1];
         // uTable defines a number of Tilieng for a shape 2 x n with extra high squre
-        long[] uTable = new long[n + 1];
         // let's define the functions
         // f(n) is a number of tilings for 2 x n board
         // How can be the last column be tiled
@@ -31,20 +25,18 @@ public class DominoAndTromino {
 
         // l(n) = f(n - 1) + u(n - 1) // last cell can be a tromino or one laying domino
         // u(n) = f(n - 1) + l(n - 1) // last cell can be a tromin0 or one laying domino
-
-        // Now let's define the base cases for all 3 functions:
+        // using substitution we get:
+        // f(n) = 2 * f(n - 1) + f (n - 3)
+        // Now let's 3 base cases
         fTable[1] = 1; // one domino
         fTable[2] = 2; // two laying or standing dominos
-        lTable[1] = 1; // one tromino
-        lTable[2] = 2; // standing domino + tromino or laying domino + tromino
-        uTable[1] = 1; // one tromino
-        uTable[2] = 2; // standing domino + tromin or laying domino + tromino
-        for (int i = 3; i <= n; i++) {
+        fTable[3] = 5; // one tromino
+        for (int i = 4; i <= n; i++) {
             // note: alternative approach here to work with substitution and get f(n) = 2 * f(n - 1) + f(n - 3)
-            fTable[i] = (fTable[i - 1] + fTable[i - 2] + uTable[i - 2] + lTable[i - 2]) % 1000000007;
-            lTable[i] = (fTable[i - 1] + uTable[i - 1]) % 1000000007;
-            uTable[i] = (fTable[i - 1] + lTable[i - 1]) % 1000000007;
+            fTable[i] = (2 * fTable[i - 1] + fTable[i - 3]) % 1000000007;
+
         }
         return Long.valueOf(fTable[n]).intValue();
     }
 }
+
