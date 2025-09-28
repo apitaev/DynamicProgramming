a99ffc8330977ceac68bf55b618420a2cc625564
Anna Pitaev <anna_pitaev@intuit.com>
2025-09-28 08:19:25 -0700

    Generated code errors for demo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DynamicProgramming
+This repository contains solutions for dynamic programming problems from leetcode.
+Please, reach to anna.pitaev@gmail.com for any questions and recommendations.
--- src/main/java/com/pitaev/leetcode/dp/decision/InterleavingString.java
+++ src/main/java/com/pitaev/leetcode/dp/decision/InterleavingString.java
@@ -14,8 +14,8 @@ public class InterleavingString {
         table[0][0] = true;
         // base cases:
         // build interleave using s2 only
-        for (int i = 1; i <= s2.length(); i++) {
-            table[0][i] = table[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
+        for (int i = 1; i <= s2.length; i++) {
+            table[0][k] = table[0][k - 1] && (s2.charAt(k - 1) == s3.charAt(i - 1));
         }
         // build interleave using s1 only
         for (int i = 1; i <= s1.length(); i++) {
